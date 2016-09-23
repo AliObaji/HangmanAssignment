1aa75fbf175e6c43c0a47e4d22afef5a636e738c
Alaa El Hariri <ashariri95@gmail.com>
2016-09-23 14:27:28 +0300

    Started working on clientTCP

--- src/clientTCP.java
+++ src/clientTCP.java
@@ -1,9 +1,48 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by ashariri95 on 9/23/2016.
  *
  * This program allows the user to initialise a game of hangman with the server, after which he/she
  * can start sending his/her guesses.
  * This client uses TCP.
+ *
+ * Used references
+ * https://systembash.com/a-simple-java-tcp-server-and-tcp-client/
+ * https://www.youtube.com/watch?v=vCDrGJWqR8w
  */
+
 public class clientTCP {
+    public static void main(String args[]) throws IOException{
+        Scanner user = new Scanner(System.in);
+
+        System.out.println("Would you like to play Hangman? Y/N?");
+        String play = user.next();
+
+        Socket clientSocket = new Socket("127.0.0.1",1024);
+        PrintStream sendToServer = new PrintStream(clientSocket.getOutputStream());
+
+        Scanner fromServer = new Scanner(clientSocket.getInputStream());
+
+        if(play.toLowerCase().equals('y')){
+            boolean playing = true;
+            sendToServer.print("PLAY");
+            String word;
+
+            while(playing) {
+                word = fromServer.next();
+
+
+            }
+
+        } else if(play.toLowerCase().equals('n')) {
+            System.out.println("Okay... Bye!");
+        } else {
+            System.out.println("LOL you funny... Bye!");
+        }
+    }
 }
--- src/serverTCP.java
+++ src/serverTCP.java
@@ -1,5 +1,12 @@
 /**
  * Created by ashariri95 on 9/23/2016.
+ *
+ * This program will be the server of the hangman application in adhering to TCP.
  */
+
 public class serverTCP {
+    public static void main(String args[]){
+
+
+    }
 }
